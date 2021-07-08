3c6cdc783caf34730c00e42166fe91fc6cd8a00d
movwf <movwf@protonmail.com>
2021-07-08 14:11:36 +0300

    Transfer amount form data set functions established.

--- index.js
+++ index.js
@@ -309,8 +309,18 @@ const App = () => {
                                     id: "jsTransferAmount",
                                     type: "number",
                                     name: "price",
+                                    onBlur: () => {
+                                      window.formData.transferAmount =
+                                        document.getElementById(
+                                          "jsTransferAmount"
+                                        ).value;
+                                      window.formData.transferCurrency =
+                                        document.getElementById(
+                                          "jsCurrencySymbol"
+                                        ).innerText;
+                                    },
                                     className:
-                                      "focus:ring-indigo-500 border-l border-b border-t border-gray-100 py-3 px-3 bg-white shadow-lg focus:border-indigo-500 block w-full pl-7 pr-12 sm:text-sm rounded-md",
+                                      "focus:ring-indigo-500 border-l border-b border-t border-gray-100 py-3 px-3 bg-white shadow-lg focus:border-indigo-500 block w-full pl-12 pr-16 sm:text-sm rounded-md",
                                     placeholder: "0.00",
                                   },
                                 },
@@ -332,6 +342,25 @@ const App = () => {
                                         type: "select",
                                         props: {
                                           id: "jsCurrency",
+                                          onChange: (e) => {
+                                            switch (e.target.selectedIndex) {
+                                              case 0:
+                                                document.getElementById(
+                                                  "jsCurrencySymbol"
+                                                ).innerText = "TL";
+                                                break;
+                                              case 1:
+                                                document.getElementById(
+                                                  "jsCurrencySymbol"
+                                                ).innerText = "USD";
+                                                break;
+                                              case 2:
+                                                document.getElementById(
+                                                  "jsCurrencySymbol"
+                                                ).innerText = "EUR";
+                                                break;
+                                            }
+                                          },
                                           name: "currency",
                                           className:
                                             "focus-within:ring-indigo-500 py-2 px-4 border-t border-r border-gray-300 border-b bo focus:border-indigo-500 h-full pl-2 pr-7 border-transparent bg-transparent text-gray-500 sm:text-sm rounded-r-md",
