975ac90ebfd804d685984a717c7b5e41cab2336a
movwf <movwf@protonmail.com>
2021-07-08 14:25:26 +0300

    3D Secure Feature added.

--- index.js
+++ index.js
@@ -520,7 +520,12 @@ const App = () => {
                               id: "jsSendButton",
                               disabled: true,
                               onClick: () => {
-                                showModal("PIN");
+                                // 3D Secure
+                                if (window.formData.transferAmount < 500) {
+                                  showModal("SUCCESS");
+                                } else {
+                                  showModal("PIN");
+                                }
                               },
                               className:
                                 "h-[40px] py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-36 transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:opacity-50 rounded-lg",
